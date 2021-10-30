368293ae0c7214dbaef5717b85967b9017c8996f
Lydia <lreitzel@gmail.com>
2021-10-30 17:54:33 -0400

    added form header

--- src/components/forms/RecipeForm.js
+++ src/components/forms/RecipeForm.js
@@ -26,6 +26,7 @@ export default class RecipeForm extends Component {
         return(
             <div className="card">
                 <form className="new-recipe-form" onSubmit={event => this.handleOnSubmit(event)}>
+                    <h4>Add New Recipe</h4>
                     <input name="cuisine_id" type="hidden" value={this.props.cuisineID} />
                     <label forHTML="title">Title:</label>
                         <input name="title" type="text" value={this.state.title} onChange={this.handleInputChange} />
