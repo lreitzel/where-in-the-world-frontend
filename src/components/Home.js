ed193893dd8ff9b908a1618d8f9e9ee94a08306a
Lydia <lreitzel@gmail.com>
2021-10-31 16:56:02 -0400

    adding style and content for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,13 @@ class Home extends Component {
         return(
             <div className="home">
                 <h2>Home Page</h2>
+                <p>
+                    If you're anything like me, during the pandemic you've discovered how much you hate trying to decide on what to make for dinner! What do I want? What do I have in the house? I just end up eating something to get rid of my hunger. But I want it to be more fun! So I came up with this website to help us find recipes from all over the world, to literally spice up our lives! Since we're very new, we only have a few reccipes and cuisines, but we're growing by the day and you can add recipes of your own!
+                </p>
+                <p>
+                    To get started, you can click on the links to left to either begin by looking at all available cuisines, or looking at every available recipe.
+                </p>
+                <p>Happy Cooking!</p>
             </div>
         )
     }
